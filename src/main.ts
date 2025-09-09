aa1134826d89088ff1eea189f9fe83a1ad51039b
i.petrenko1 <i.petrenko@libersave.com>
2025-09-09 15:10:26 +0300

    Добавленыв комментарии для гварда

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use((req, res, next) => {
+    console.log('--- Incoming Request ---');
+    console.log('Protocol:', req.protocol); // http или https
+    console.log('Headers X-Forwarded-Proto:', req.headers['x-forwarded-proto']);
+    console.log('Secure:', req.secure); // true, если https
+    console.log('Host:', req.headers.host);
+    console.log('Cookies:', req.headers.cookie);
+    console.log('-------------------------');
+    next();
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -17,17 +27,6 @@ async function bootstrap() {
 
   app.set('trust proxy', 1);
   app.use(cookieParser());
-
-  app.use((req, res, next) => {
-    console.log('--- Incoming Request ---');
-    console.log('Protocol:', req.protocol); // http или https
-    console.log('Headers X-Forwarded-Proto:', req.headers['x-forwarded-proto']);
-    console.log('Secure:', req.secure); // true, если https
-    console.log('Host:', req.headers.host);
-    console.log('Cookies:', req.headers.cookie);
-    console.log('-------------------------');
-    next();
-  });
 
   app.enableCors({
     origin: [
